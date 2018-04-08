33902a9fda6bf80d3ef0bb76580bbcc3c2025125
MakeHoney <makehoney3@gmail.com>
2018-04-08 21:26:12 +0900

    autoload 관련 사항 readme수정

--- lib/crawl.rb
+++ lib/crawl.rb
@@ -1,49 +1,49 @@
 require 'open-uri'
 module Crawl
-class SchoolFood
-	@html; @page
+	class SchoolFood
+		@html; @page
 
-	def initialize 
-		@url = 'http://www.ajou.ac.kr/kr/life/food.jsp'
-		@html = open(@url).read
-		# @html = HTTP.get('http://www.ajou.ac.kr/kr/life/food.jsp').to_s
-		@html = fixHtml(@html)
-		@page = Nokogiri::HTML(@html)
-	end
+		def initialize 
+			@url = 'http://www.ajou.ac.kr/kr/life/food.jsp'
+			@html = open(@url).read
+			# @html = HTTP.get('http://www.ajou.ac.kr/kr/life/food.jsp').to_s
+			@html = fixHtml(@html)
+			@page = Nokogiri::HTML(@html)
+		end
 
-	def fixHtml(html)
-		html.gsub!(/<[가-힣]/) {|s| s = '&lt;' + s[1]}
-		html.gsub!(/[가-힣]>/) {|s| s = s[0] + '&gt;'}
-	end
-	
-	def studentFoodCourt
-		@page.css('table.ajou_table')[0].css('td.no_right li').each do |li|
-			puts li.text
-		end	
-	end
-
-
-	def dormFoodCourt
-		retStr =""
-		time = ['아침', '점심', '저녁']
-
-		3.times do |i|
-			retStr += "<p><li>******** #{time[i]} ********</li></p>"
-			@page.css('table.ajou_table')[1].
-			css('td.no_right')[i + 1].		# 아침 점심 저녁 선택자
-
-			css('li').each do |li|
-				retStr += "<li>#{li.text}</li>"
+		def fixHtml(html)
+			html.gsub!(/<[가-힣]/) {|s| s = '&lt;' + s[1]}
+			html.gsub!(/[가-힣]>/) {|s| s = s[0] + '&gt;'}
+		end
+		
+		def studentFoodCourt
+			@page.css('table.ajou_table')[0].css('td.no_right li').each do |li|
+				puts li.text
 			end	
 		end
-		return fixHtml(retStr)
-	end
 
 
-	def wholeList
-		page.css('table.ajou_table li').each do |li|
-		puts li.text
-		end		
+		def dormFoodCourt
+			retStr =""
+			time = ['아침', '점심', '저녁']
+
+			3.times do |i|
+				retStr += "<p><li>******** #{time[i]} ********</li></p>"
+				@page.css('table.ajou_table')[1].
+				css('td.no_right')[i + 1].		# 아침 점심 저녁 선택자
+
+				css('li').each do |li|
+					retStr += "<li>#{li.text}</li>"
+				end	
+			end
+			return fixHtml(retStr)
+		end
+
+
+		def wholeList
+			page.css('table.ajou_table li').each do |li|
+			puts li.text
+			end		
+		end
 	end
-end
 end
